a55fc68908a314498bd5e19f2635499374b20146
Ziyang Li <zli317@ucsc.edu>
2022-04-13 14:33:57 -0700

    1

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ let config = {
 
 let game = new Phaser.Game(config);
 
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
+
 // set UI sizes
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
